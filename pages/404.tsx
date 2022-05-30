03c5ac341f07826ff1cd3279a4b35f367a3ac94d
Tine Krüger <krueger.christ@gmail.com>
2022-05-30 14:53:52 +0200

    why is about me page not showing up?

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,5 +1,9 @@
 import { Error } from "../components/error/error";
+import { NGColor } from "../types/colors";
 
 export default function Custom404() {
     return <Error errorText="Upps, diese Seite konnten wir nicht finden." errorType="Error 404"/>
   }
+
+  Custom404.footerClass = 'c-bg--blue'
+  Custom404.headerColor = NGColor.petrol
