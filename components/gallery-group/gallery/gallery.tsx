3281612c0438a72775d87050920aa692fdbf98b1
Christine Krüger <christine.krueger@meltingelements.com>
2022-12-06 22:38:18 +0100

    refactored gallery

--- components/gallery-group/gallery/gallery.tsx
+++ components/gallery-group/gallery/gallery.tsx
@@ -1,49 +1,51 @@
-import classNames from "classnames";
-import { ComponentProps, CSSProperties, FC, memo, useEffect, useState } from "react";
+import classNames from 'classnames'
+import dynamic from 'next/dynamic'
+import { ComponentProps, FC, memo, useState } from 'react'
+import { uid } from 'react-uid'
+import { NGColor } from '../../../types/colors'
+import { CustomImage, ICustomImageProps } from '../../custom-image/custom-image'
+import { LightboxImage } from '../../lightbox/lightbox-image/lightbox-image'
+import { IPostProps } from '../gallery-wrapper'
 import styles from './gallery.module.css'
-import { CustomImage, ICustomImageProps, wordpressLoader } from '../../custom-image/custom-image'
-import { uid } from "react-uid";
-import { NGColor } from "../../../types/colors";
-import { LightboxImage } from "../../lightbox/lightbox-image/lightbox-image";
-import { IPostProps } from "../gallery-wrapper";
-import dynamic from "next/dynamic";
 
 export interface IGalleryProps extends ComponentProps<'div'> {
-    images: IPostProps[]
-    highlightColor?: NGColor
-    texts?: string[]
+	images: IPostProps[]
+	highlightColor?: NGColor
+	texts?: string[]
 }
 
-const LightboxComponent = dynamic(() => import('../../lightbox/lightbox'));
+const LightboxComponent = dynamic(() => import('../../lightbox/lightbox'))
 
 const UnmemoizedGallery: FC<IGalleryProps> = (props) => {
-    const {className, children, images, highlightColor, texts,  ...attributes} = props
-    const classes = classNames(className, styles.container,  'grid', {[`c-hili--${highlightColor}`]  : highlightColor})
-    const [lightboxIsOpen, setLightboxIsOpen] = useState<boolean>(false)
-    const [imageToShow, setImagetoShow] =useState<ICustomImageProps | undefined>()
+	const { className, children, images, highlightColor, texts, ...attributes } = props
+	const classes = classNames(className, styles.container, 'grid', { [`c-hili--${highlightColor}`]: highlightColor })
+	const [lightboxIsOpen, setLightboxIsOpen] = useState<boolean>(false)
+	const [imageToShow, setImagetoShow] = useState<ICustomImageProps | undefined>()
 
-   
-    return (
-        <div className={classes} {...attributes}>
-            {images.map( (image, i) => (
-                    <div key={uid(image)} className={styles[`image-wrapper`]} onClick={() => handleImageClick(image.image)}>
-                        <CustomImage className={styles.image} objectFit={'cover'} sizes={'(min-width: 1280px) 25vw, (min-width: 960px) 33.3vw, (min-width: 768px) 70vw, (min-width: 680px) 50vw , 100vw'} {...image.image} width={undefined} height={undefined} />
-                    </div>
-            ))}
-           
-            {texts && texts.map( (text, i) => 
-               {return text !==  null ?  (<div key={uid(i)} className={styles[`text-${i + 1}`]}>
-                    <p>{text}</p>
-                </div> ) : undefined }  
-            )}
-            {lightboxIsOpen && imageToShow && <LightboxComponent setOpen={setLightboxIsOpen} content={<LightboxImage {...imageToShow}/>}/>}
-        </div>
-    )
+	return (
+		<div className={classes} {...attributes}>
+			{images.map((image, i) => (
+				<div key={uid(image)} className={styles[`image-wrapper`]} onClick={() => handleImageClick(image.image)}>
+					<CustomImage className={styles.image} objectFit={'cover'} sizes={'(min-width: 1280px) 25vw, (min-width: 960px) 33.3vw, (min-width: 768px) 70vw, (min-width: 680px) 50vw , 100vw'} {...image.image} width={undefined} height={undefined} />
+				</div>
+			))}
 
-    function handleImageClick(image: ICustomImageProps) {
-        setLightboxIsOpen(true)
-        setImagetoShow(image)
-    }
+			{texts &&
+				texts.map((text, i) => {
+					return text !== null ? (
+						<div key={uid(i)} className={styles[`text-${i + 1}`]}>
+							<p>{text}</p>
+						</div>
+					) : undefined
+				})}
+			{lightboxIsOpen && imageToShow && <LightboxComponent setOpen={setLightboxIsOpen} content={<LightboxImage {...imageToShow} />} />}
+		</div>
+	)
+
+	function handleImageClick(image: ICustomImageProps) {
+		setLightboxIsOpen(true)
+		setImagetoShow(image)
+	}
 }
 
 export const Gallery = memo(UnmemoizedGallery)
