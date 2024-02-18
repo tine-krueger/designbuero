c5357ab02881d5cc29daf5cd35d95bdcffbc3fa1
Christine Krüger <christine.krueger@meltingelements.com>
2024-02-18 22:28:53 +0100

    review changes

--- components/workshops/workshop/workshop.tsx
+++ components/workshops/workshop/workshop.tsx
@@ -36,7 +36,7 @@ export const WorkShop: FC<WorkshopProps> = (props) => {
 				as="link"
 				link={{
 					type: 'mail',
-					href: `mailto:ng@desingbuero.de?subject=Anfrage%20Portfolio&body=Hallo%20Frau%20Giesler%2C%20%0A%0Ahiermit%20m%C3%B6chte%20ich%20mich%20verbindlich%20an%20folgendem%20Workshop%20anmelden%3A%0A%0ATitel%3A%20${title}%0ADatum%3A%20${startDate}%20%0AUhrzeit%3A%20${startTime}%20Uhr%20`,
+					href: `mailto:ng@desingbuero.de?subject=Anfrage%20Workshop&body=Hallo%20Frau%20Giesler%2C%20%0A%0Ahiermit%20m%C3%B6chte%20ich%20mich%20verbindlich%20an%20folgendem%20Workshop%20anmelden%3A%0A%0ATitel%3A%20${title}%0ADatum%3A%20${startDate}%20%0AUhrzeit%3A%20${startTime}%20Uhr%20%20%0A%0ABitte%20senden%20Sie%20mir%20alle%20weiteren%20Infos%20zum%20Workshop%20zu.`,
 					target: '_blank',
 				}}
 			>
