5beec6c9eaa7c6052a0de0a16254a44a5a78ad5a
Christine Krüger <christine.krueger@meltingelements.com>
2024-04-09 21:53:04 +0200

    current year

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -15,12 +15,14 @@ export const UnmemoizedFooter: FC<IFooterProps> = (props) => {
 	const { className, ...attributes } = props
 	const { globalMenuState, lightbox } = useContext(globalMenuContext)
 	const classes = classNames(className, styles.container)
+
+	const date = new Date().getFullYear()
 	return (
 		<footer className={classes} {...attributes}>
 			<div className={classNames(styles['inner-wrapper'], 'grid')}>
 				<div className={classNames(styles.copyright, 'flex')}>
 					<CustomImage className={styles.logo} alt={'Logo Desingbürp'} src={'/assets/svg/Logo_desingbuero.svg'} />
-					<p className={classNames('c-t--white font-style--xs')}>© 2022</p>
+					<p className={classNames('c-t--white font-style--xs')}>© {date}</p>
 				</div>
 				<Navigation className={classNames(styles['legal-nav'], 'font-style--xs')} childElementsClasses={{ listItem: styles['legal-nav-item'] }} aria-label={'legal'} navlist={globalMenuState.legal} />
 				{/* <Navigation className={styles['social-nav']} childElementsClasses={{listItem: styles['social-nav-item']}} aria-label={'social'} navlist={globalMenuState.socialNav}/> */}
